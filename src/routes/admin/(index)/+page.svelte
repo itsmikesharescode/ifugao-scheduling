4f5d2d8ca0dda2bc10e4174dc38a678a9c3d3c50
Mike John Eviota <eviotamikejohnb@gmail.com>
2025-02-02 13:11:25 +0800

    add subject table

--- src/routes/admin/(index)/+page.svelte
+++ src/routes/admin/(index)/+page.svelte
@@ -1,1 +1,18 @@
-wew
+<script lang="ts">
+  import DataTable from '$lib/components/ui/data-table/data-table.svelte';
+  import { columns } from './components/table/columns';
+</script>
+
+<main>
+  <DataTable
+    data={[
+      {
+        id: 2,
+        created_at: 'qweqwe',
+        course_code: 'GEO101',
+        name: 'Analytical Geometry'
+      }
+    ]}
+    {columns}
+  />
+</main>
---
+++ src/routes/admin/(index)/components/table/row-actions.svelte
@@ -0,0 +1,36 @@
+<script lang="ts" module>
+  type TData = unknown;
+</script>
+
+<script lang="ts" generics="TData">
+  import Ellipsis from 'lucide-svelte/icons/ellipsis';
+  import type { Row } from '@tanstack/table-core';
+  import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+  import Button from '$lib/components/ui/button/button.svelte';
+  import FilePenLine from 'lucide-svelte/icons/file-pen-line';
+  import FileX2 from 'lucide-svelte/icons/file-x-2';
+
+  let { row }: { row: Row<TData> } = $props();
+</script>
+
+<DropdownMenu.Root>
+  <DropdownMenu.Trigger>
+    {#snippet child({ props })}
+      <Button {...props} variant="ghost" class="flex h-8 w-8 p-0 data-[state=open]:bg-muted">
+        <Ellipsis />
+        <span class="sr-only">Open Menu</span>
+      </Button>
+    {/snippet}
+  </DropdownMenu.Trigger>
+  <DropdownMenu.Content class="w-[160px]" align="end">
+    <DropdownMenu.Item>
+      Edit
+      <FilePenLine class="ml-auto" />
+    </DropdownMenu.Item>
+    <DropdownMenu.Separator />
+    <DropdownMenu.Item>
+      <span class="text-destructive">Delete</span>
+      <FileX2 class="ml-auto text-destructive" />
+    </DropdownMenu.Item>
+  </DropdownMenu.Content>
+</DropdownMenu.Root>
