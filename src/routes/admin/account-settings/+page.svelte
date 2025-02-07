334fa37566d49483d7be0447b5583da86c946d4a
Mike John Eviota <eviotamikejohnb@gmail.com>
2025-02-07 22:14:45 +0800

    add template

--- src/routes/admin/account-settings/+page.svelte
+++ src/routes/admin/account-settings/+page.svelte
@@ -1,1 +1,6 @@
-wew
+<script lang="ts">
+</script>
+
+<main class="flex min-h-screen items-center justify-center">
+  <span class="rounded-lg bg-secondary p-4">Still on-going work👷👷‍♀️</span>
+</main>
